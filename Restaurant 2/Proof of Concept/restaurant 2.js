d9e44d43616df7d6ab9aa719a834df5edf711a03
Garrett Ellison <garrett.e.ellison@icloud.com>
2021-02-01 10:40:47 -0600

    Completed

--- Restaurant 2/Proof of Concept/restaurant 2.js
+++ Restaurant 2/Proof of Concept/restaurant 2.js
@@ -1,23 +1,36 @@
-var menuitem;
-var orders;
+var menuitem ="a";
+var orders = [];
 
 function viewCart(item) {
   menuitem = item;
   var view = document.getElementsByClassName("btn");
   view[0].style.display = "table";
   changeButton();
+  addToOrder();
 
   return;
 };
 
 function changeButton() {
-  var x = document.getElementById(menuitem);
+  var x = document.getElementById("item"+menuitem);
   x.className = "in-cart";
   x.textContent = "Added to Cart!";
   return;
  };
 
- function addOrder() {
+ function addToOrder() {
+    var item = document.querySelector("#item"+menuitem);
+    // alert(item.dataset.item+" "+item.dataset.price);
+    
+    var order = {
+      item: item.dataset.item,
+      price: item.dataset.price
+    };
+    orders.push(order);
+    return;
+ };
 
-  return;
+ function showCart() {
+   sessionStorage.setItem("orders", JSON.stringify(orders));
+   location.href = "restaurant 2 order.html";
  };
