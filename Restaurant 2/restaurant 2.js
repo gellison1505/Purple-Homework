c66f60efe0333212d8b38a2a91262bba54da4470
Garrett Ellison <garrett.e.ellison@icloud.com>
2021-02-01 11:34:41 -0600

    Proposed Student Code

---
+++ Restaurant 2/restaurant 2.js
@@ -0,0 +1,19 @@
+var menuitem ="a";
+var orders = [];
+
+function addToCart(item) {
+  
+};
+
+function changeButton() {
+  
+ };
+
+ function addToOrder() {
+    
+ };
+
+ function showCart() {
+   sessionStorage.setItem("orders", JSON.stringify(orders));
+   location.href = "restaurant 2 order.html";
+ };
