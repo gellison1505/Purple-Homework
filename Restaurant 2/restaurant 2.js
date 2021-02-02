eb4074c0bbd77c305a2a2e33195d60fd5bc9a90d
Garrett Ellison <garrett.e.ellison@icloud.com>
2021-02-02 17:45:15 -0600

    Revised based on Feb. 2 meeting

--- Restaurant 2/restaurant 2.js
+++ Restaurant 2/restaurant 2.js
@@ -1,17 +1,34 @@
-var menuitem ="a";
+var menuitem ="";
 var orders = [];
 
-function addToCart(item) {
+// handle menu item click event
+function handleItem() {
+  addToCart();
+  changeButton();
+  addToOrder();
+}
+
+// unhide Shopping Cart icon
+function showCartIcon(item) {
   
 };
 
+// change menu name of clicked button to "Added to Cart!"
 function changeButton() {
   
  };
 
+ // a) create an object containing the name and price of the item;
+ // b) store object data as key-value pairs
+ // c) store object in the global orders[]; 
  function addToOrder() {
     
  };
+
+ // !! DO NOT MODIFY OR DELETE 
+ // a) handle Shopping Cart click event
+ // b) store orders[] to a local object called sessionStorage
+ // c) open page "restaurant 2 order.html"
 
  function showCart() {
    sessionStorage.setItem("orders", JSON.stringify(orders));
